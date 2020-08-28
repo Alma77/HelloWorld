5c40ac9efcdf6eaf698f10ccdef30b2b47cab121
Tanner Trimble <mrtannertrimble@gmail.com>
2020-08-28 14:00:55 -0600

    Updated the layout and made progress on functions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,9 +15,21 @@ namespace HelloWorld.Controllers
             return View();
         }
 
+        [HttpGet("Home/[action]", Name = "Link")]
+        public IActionResult NewPage()
+        {
+            return View();
+        }
+        
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Link()
+        {
+            var url = Url.RouteUrl("Link", new{}, Request.Scheme);
+            return Content(url);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/Comment.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HelloWorld.Models
+{
+    public class Comment
+    {
+        public string Name{get;set;}
+        public string Content{get;set;}
+    }
+}
